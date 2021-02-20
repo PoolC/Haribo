b24cf462dd7dafe6f0a844c3f6030b41578728f2
mingd1023 <mingd1023@gamail.com>
2021-02-20 20:28:43 +0900

    Feat: 필요한 페이지들 생성

---
+++ src/pages/ApplyPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ApplyPage = () => {
+  return <></>;
+};
+
+export default ApplyPage;
---
+++ src/pages/admin/AdminHomePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AdminHomePage = () => {
+  return <></>;
+};
+
+export default AdminHomePage;
