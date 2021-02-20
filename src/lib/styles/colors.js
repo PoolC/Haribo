3fad99237a84cc9cafd44845d2178ade70959ac7
mingd1023 <mingd1023@gamail.com>
2021-02-20 20:25:44 +0900

    Feat: 자주 사용하는 색상코드 분리

---
+++ src/lib/styles/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  // mint: ['rgb(252, 237, 239)', 'rgb(253, 215, 220)', 'rgb(255, 195, 203)'],
+  mint: ['#e5f0ed', '#a7dacb', '#47be9b', '#9EDEC6', '#D2E5D0'],
+  brown: ['#817971', '#4c3722'],
+  red: ['rgb(250, 141, 141)'],
+  white: ['rgba(255, 255, 255, 0)'],
+  gray: ['rgb(250, 250, 250)', 'rgba(0, 0, 0, 0.035)', 'rgba(0, 0, 0, 0.075)'],
+};
+
+export default colors;
