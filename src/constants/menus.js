f4f6300801121c464d32b157ea7c6683e95a3d4d
mingd1023 <mingd1023@gamail.com>
2021-02-20 20:26:46 +0900

    Feat: 메뉴들과 그 url 정보 따로 분리

---
+++ src/constants/menus.js
@@ -0,0 +1,15 @@
+const MENU = {
+  INTRO: 'intro',
+  MEMBERS: 'members',
+  BOARDS: 'boards',
+  PROJECTS: 'projects',
+  ACTIVITIES: 'activities',
+  BOOKS: 'books',
+  APPLY: 'apply',
+  ADMIN: 'admin',
+  MYINFO: 'my-info',
+  SIGNIN: 'login',
+  SIGNUP: 'register',
+};
+
+export { MENU };
