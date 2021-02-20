f499358bd98fda7a76cd94e20ed893e0fd32ea12
mingd1023 <mingd1023@gamail.com>
2021-02-20 20:27:02 +0900

    Feat: 회원가입 폼 컨테이너 구현

---
+++ src/containers/auth/RegisterFormContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AuthForm from '../../components/auth/AuthForm';
+
+const RegisterFormContainer = () => {
+  return <AuthForm type="register" />;
+};
+
+export default RegisterFormContainer;
