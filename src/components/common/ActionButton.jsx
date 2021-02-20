d2fcef5759b4534884203b064a7426c0e23f9a6e
mingd1023 <mingd1023@gamail.com>
2021-02-20 20:21:58 +0900

    Feat: 공통 버튼 컴포넌트 구현

---
+++ src/components/common/ActionButton.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import colors from '../../lib/styles/colors';
+
+const StyledActionButton = styled.button`
+  background-color: ${colors.mint[1]};
+  color: ${colors.gray[0]};
+  padding: 5px 12px;
+  margin: 0 10px;
+  border-radius: 8px;
+  transition: 0.3s;
+  font-weight: 700;
+  &:hover {
+    background-color: ${colors.mint[2]};
+    transition: 0.3s;
+  }
+`;
+
+const StyledActionButtonLink = styled(Link)`
+  background-color: ${colors.mint[1]};
+  color: ${colors.gray[0]};
+  padding: 8px 12px;
+  margin: 0 10px;
+  border-radius: 8px;
+  transition: 0.3s;
+  font-weight: 700;
+  font-size: 0.8rem;
+  &:hover {
+    background-color: ${colors.mint[2]};
+    transition: 0.3s;
+  }
+`;
+
+const ActionButton = (props) => {
+  return props.to ? (
+    <StyledActionButtonLink {...props} />
+  ) : (
+    <StyledActionButton {...props} />
+  );
+};
+
+export default ActionButton;
