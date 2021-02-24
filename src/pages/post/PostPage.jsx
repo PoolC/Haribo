cb79cd3c1eff3f74f0e505b39ee6de61aab48d54
mingd1023 <mingd1023@gamail.com>
2021-02-24 18:29:03 +0900

    Feat: 포스트 관련 페이지 구현

---
+++ src/pages/post/PostPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PostContainer from '../../containers/board/PostContainer/PostContainer';
+
+const PostPage = () => {
+  return <PostContainer />;
+};
+
+export default PostPage;
---
+++ src/pages/post/PostWritePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BoardMenu from '../../components/board/BoardMenu/BoardMenu';
+import PostFormContainer from '../../containers/board/PostFormContainer/PostFormContainer';
+
+const PostWritePage = () => {
+  return (
+    <>
+      <PostFormContainer />
+    </>
+  );
+};
+
+export default PostWritePage;
