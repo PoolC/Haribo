b9a3019e14831ede6f40f22221a98de24656f2e1
mingd1023 <mingd1023@gamail.com>
2021-02-25 17:26:40 +0900

    Refactor: 사용하지 않는 import 제거

--- src/pages/post/PostWritePage.jsx
+++ src/pages/post/PostWritePage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import BoardMenu from '../../components/board/BoardMenu/BoardMenu';
 import PostFormContainer from '../../containers/board/PostFormContainer/PostFormContainer';
 
 const PostWritePage = () => {
