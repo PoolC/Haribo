b81c287a63974271adf0e5b2f7bbcb8f08f61a4e
mingd1023 <mingd1023@gamail.com>
2021-02-25 17:25:12 +0900

    Feat: useInput custom hook 구현
    회원가입/로그인/정보 수정 시 사용

---
+++ src/hooks/useInput.js
@@ -0,0 +1,16 @@
+import { useCallback, useState } from 'react';
+
+export default (initialValue = null, condition) => {
+  const [value, setValue] = useState(initialValue);
+  const [error, setError] = useState(false);
+  const handler = useCallback((e) => {
+    setValue(e.target.value);
+    if (!condition(e.target.value)) {
+      setError(true);
+    }
+    if (condition(e.target.value)) {
+      setError(false);
+    }
+  }, []);
+  return [value, handler, error];
+};
