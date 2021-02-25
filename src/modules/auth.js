65bfe176f91ed7d4d25420b79680e5e08b0d018f
mingd1023 <mingd1023@gamail.com>
2021-02-25 17:26:19 +0900

    Feat: initial state 설정

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -4,7 +4,20 @@ const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
 
 export const sampleAction = createAction(SAMPLE_ACTION);
 
-const initialState = {};
+const initialState = {
+  login: {
+    status: 'INIT',
+  },
+  status: {
+    isLogin: false,
+    isAdmin: false,
+    init: false,
+  },
+  user: {
+    id: '',
+    password: '',
+  },
+};
 
 const auth = handleActions(
   {
