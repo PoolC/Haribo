d0cbdf3ae0104ff57edc2ef453f97006b699ec86
mingd1023 <mingd1023@gamail.com>
2021-02-25 17:27:10 +0900

    Feat: 회원가입 페이지 라우팅 추가

--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import RegisterFormContainer from '../../containers/auth/RegisterFormContainer/RegisterFormContainer';
+import { Route, Switch } from 'react-router-dom';
+import { MENU } from '../../constants/menus';
+import RegisterSuccess from '../../components/auth/RegisterSuccess';
 
 const RegisterPage = () => {
   return (
     <>
-      <RegisterFormContainer />
+      <>
+        <Switch>
+          <Route exact path="/register/success" component={RegisterSuccess} />
+          <Route
+            component={RegisterFormContainer}
+            path={`/${MENU.SIGNUP}`}
+            exact
+          />
+          {/* <Route
+          exact
+          path="/register"
+          render={() => (
+            <Authentication mode={2} handleRegister={this.handleRegister} />
+          )}
+        />
+        <Route component={NotFound} /> */}
+        </Switch>
+      </>
     </>
   );
 };
