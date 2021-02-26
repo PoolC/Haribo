572485f40e22082bcede895fe501344496c94df5
mingd1023 <mingd1023@gmail.com>
2021-02-27 03:22:41 +0900

    Feat: 멤버 관련 api 정리

--- src/lib/api/member.js
+++ src/lib/api/member.js
@@ -2,3 +2,15 @@ import client from './client';
 
 export const searchMember = ({ name }) =>
   client.get(`/project/member?name=${name}`);
+
+export const getMembers = () => client.get('/member');
+
+export const getMember = (loginID) => client.get(`/member/${loginID}`);
+
+export const acceptMember = (loginID) =>
+  client.put(`/member/activate/${loginID}`);
+
+export const withdrawMember = (loginID) => client.delete(`/member/${loginID}`);
+
+export const toggleAdmin = ({ loginID, isAdmin }) =>
+  client.put(`/member/admin/${loginID}`, isAdmin);
