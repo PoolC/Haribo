8f46e53c4d5150a8a2955cf90e2fc6e9fc67cdb5
mingd1023 <mingd1023@gamail.com>
2021-02-26 22:52:06 +0900

    Feat: 404 페이지 구현

---
+++ src/pages/error/NotFoundPage.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+import colors from '../../lib/styles/colors';
+
+export const PageBlock = styled.div`
+  position: relative;
+  top: 0px;
+  left: 0;
+  right: 0;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  margin: 0px 0 300px 0;
+  @media (max-width: 576px) {
+    margin-bottom: 500px;
+  }
+`;
+
+export const PageContainer = styled.div`
+  width: 100%;
+  margin: 0 5%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  box-shadow: 0px 0px 20px ${colors.gray[1]};
+  border-radius: 50px;
+  padding: 40px 40px 60px 40px;
+  min-height: 60vh;
+`;
+
+const ErrorIcon = styled.i`
+  font-size: 3rem;
+  color: ${colors.red[0]};
+  margin-bottom: 1rem;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 1.2rem;
+  line-height: 1.5rem;
+  text-align: center;
+  word-break: keep-all;
+  font-weight: 600;
+`;
+
+const NotFoundPage = () => {
+  return (
+    <PageBlock>
+      <PageContainer>
+        <ErrorIcon className="fas fa-exclamation-circle"></ErrorIcon>
+        <ErrorMessage>존재하지 않는 페이지입니다.</ErrorMessage>
+      </PageContainer>
+    </PageBlock>
+  );
+};
+
+export default NotFoundPage;
