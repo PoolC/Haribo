1d9634ffa4571c6e384b3ae0794ef642795e360a
mingd1023 <mingd1023@gmail.com>
2021-02-27 17:50:22 +0900

    Feat: activity api 정리

---
+++ src/lib/api/activity.js
@@ -0,0 +1,87 @@
+import client from './client';
+
+export const getActivities = () => client.get('/activity');
+
+export const getActivityYears = () => client.get('/activity/years');
+
+export const getActivity = (activityID) =>
+  client.get(`/activity/${activityID}`);
+
+export const createActivity = ({
+  title,
+  description,
+  startDate,
+  isSeminar,
+  classHour,
+  capacity,
+  tags,
+}) =>
+  client.post(`/activity`, {
+    title,
+    description,
+    startDate,
+    isSeminar,
+    classHour,
+    capacity,
+    tags,
+  });
+
+export const updateActivity = ({
+  activityID,
+  title,
+  description,
+  startDate,
+  isSeminar,
+  classhour,
+  capacity,
+  tags,
+}) =>
+  client.put(`/activity/${activityID}`, {
+    title,
+    description,
+    startDate,
+    isSeminar,
+    classhour,
+    capacity,
+    tags,
+  });
+
+export const deleteActivity = (activityID) =>
+  client.delete(`activity/${activityID}`);
+
+export const createActivitySession = ({
+  activityID,
+  sessionNumber,
+  date,
+  description,
+}) =>
+  client.post('/activity/session', {
+    activityID,
+    sessionNumber,
+    date,
+    description,
+  });
+
+export const getActivitySessions = (activityID) =>
+  client.get(`/activity/session/${activityID}`);
+
+export const updateActivitySession = ({ date, description }) =>
+  client.post(`/activity/session/${sessionID}`, {
+    date,
+    description,
+  });
+
+export const applyActivity = (activityID) =>
+  client.post(`/activity/apply/${activityID}`);
+
+export const getActivityMembers = (activityID) =>
+  client.get(`/activity/member/${activityID}`);
+
+export const getActivitySessionAttendances = (sessionID) =>
+  client.get(`/activity/check/${sessionID}`);
+
+export const checkActivityAttendance = ({ sessionID, members }) =>
+  client.post('/activity/check', {
+    sessionID,
+    members,
+  });
