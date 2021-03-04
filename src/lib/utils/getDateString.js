0e02e541c959e25fc2275732855443806b9ff1b2
mingd1023 <mingd1023@gamail.com>
2021-03-04 20:01:13 +0900

    Feat: 날짜와 시간 문자열 생성하는 함수 구현

--- src/lib/utils/getDateString.js
+++ src/lib/utils/getDateString.js
@@ -1,5 +1,5 @@
-export const getFullCurrentDateString = () => {
-  const currentDate = new Date();
+export const getFullCurrentDateString = (time) => {
+  const currentDate = time ? new Date(time) : new Date();
   const month =
     currentDate.getMonth() < 10
       ? `0${currentDate.getMonth()}`
@@ -9,4 +9,21 @@ export const getFullCurrentDateString = () => {
       ? `0${currentDate.getDate()}`
       : currentDate.getDate();
   return `${currentDate.getFullYear()}-${month}-${date}`;
+};
+
+export const getFullCurrentDateTimeString = (time) => {
+  const currentDate = time ? new Date(time) : new Date();
+  const month =
+    currentDate.getMonth() < 10
+      ? `0${currentDate.getMonth()}`
+      : currentDate.getMonth();
+  const date =
+    currentDate.getDate() < 10
+      ? `0${currentDate.getDate()}`
+      : currentDate.getDate();
+  const hour = currentDate.getHours();
+  const minute = currentDate.getMinutes();
+  const second = currentDate.getSeconds();
+
+  return `${currentDate.getFullYear()}-${month}-${date} ${hour}:${minute}:${second}`;
 };
