65784ba5056fd1aee82275848790d2ec395bcbbf
mingd1023 <mingd1023@gamail.com>
2021-03-05 19:07:05 +0900

    Fix: 잘못된 권한 체크 함수 삭제

--- src/pages/member/MemberDetailPage.jsx
+++ src/pages/member/MemberDetailPage.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import MemberDetailContainer from '../../containers/member/MemberDetailContainer/MemberDetailContainer';
-import { pageAuthCheckByPermission } from '../../lib/utils/authCheck';
 import { useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 const MemberDetailPage = ({ history }) => {
   const member = useSelector((state) => state.auth);
-
-  pageAuthCheckByPermission(history, member, 'MEMBER');
 
   return <MemberDetailContainer />;
 };
--- src/pages/member/MemberListPage.jsx
+++ src/pages/member/MemberListPage.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import MemberListContainer from '../../containers/member/MemberListContainer/MemberListContainer';
-import { pageAuthCheckByPermission } from '../../lib/utils/authCheck';
 
 const MemberListPage = ({ history }) => {
   const member = useSelector((state) => state.auth);
-
-  pageAuthCheckByPermission(history, member, 'MEMBER');
 
   return <MemberListContainer />;
 };
