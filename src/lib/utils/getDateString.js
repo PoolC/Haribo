031f3d48a5aeea8654bd620a98fd8ddcc3672f03
mingd1023 <mingd1023@gamail.com>
2021-03-05 21:31:05 +0900

    Fix: 시간에 대해서도 적절히 0을 추가하도록 수정

--- src/lib/utils/getDateString.js
+++ src/lib/utils/getDateString.js
@@ -21,9 +21,18 @@ export const getFullCurrentDateTimeString = (time) => {
     currentDate.getDate() < 10
       ? `0${currentDate.getDate()}`
       : currentDate.getDate();
-  const hour = currentDate.getHours();
-  const minute = currentDate.getMinutes();
-  const second = currentDate.getSeconds();
+  const hour =
+    currentDate.getHours() < 10
+      ? `0${currentDate.getHours()}`
+      : currentDate.getHours();
+  const minute =
+    currentDate.getMinutes() < 10
+      ? `0${currentDate.getMinutes()}`
+      : currentDate.getMinutes();
+  const second =
+    currentDate.getSeconds() < 10
+      ? `0${currentDate.getSeconds()}`
+      : currentDate.getSeconds();
 
   return `${currentDate.getFullYear()}-${month}-${date} ${hour}:${minute}:${second}`;
 };
