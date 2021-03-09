e30737f484aa6d428f284ab62215bddd47072a52
mingd1023 <mingd1023@gamail.com>
2021-03-09 16:33:17 +0900

    Feat: 파일 업로드를 위한 컴포넌트 구현

--- src/components/common/Buttons/FileUploadButton.jsx
+++ src/components/common/Buttons/FileUploadButton.jsx
@@ -1,91 +1,81 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import colors from '../../../lib/styles/colors';
 import ActionButton from './ActionButton';
 import * as fileAPI from '../../../lib/api/file';
-import { set } from '../../../../node_modules/stylis/stylis';
+import FileUploadModal from '../FileUploadModal/FileUploadModal';
+import Modal from '../Modal/Modal';
 
-export const StyledFileButton = styled.label`
-  &.browse-file {
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    background-color: ${colors.mint[1]};
-    color: ${colors.gray[0]};
-    border-radius: 8px;
-    transition: 0.3s;
-    font-weight: 700;
-    font-size: 0.8rem;
-    max-width: 5rem;
-    padding: 0.5rem;
-    margin: 1rem 0 0 0;
-    &:hover {
-      background-color: ${colors.mint[2]};
-      transition: 0.3s;
-    }
-  }
-`;
-
-export const StyledFileInput = styled.input`
-  position: relative;
-  width: 1px;
-  height: 1px;
-  padding: 0;
-  margin: -1px;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  border: 0;
-  outline: 0;
-`;
-
-const FileName = styled.p`
-  margin: 1rem 0 0 0;
-`;
-
-const StyledActionButton = styled(ActionButton)`
-  margin: 1rem 0 0 0;
-`;
-
-const FileUploadButton = ({ file, onChangeFunc }) => {
+const FileUploadButton = ({ onSubmit }) => {
   let formData = new FormData();
 
-  const [imageUrl, setImageUrl] = useState('');
+  const [file, setFile] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [errorModalVisible, setErrorModalVisible] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  const handleChange = (e) => {
-    onChangeFunc(e.target.files[0]);
+  const onBrowseFile = (e) => {
+    e.preventDefault();
+    setFile(e.target.files[0]);
   };
-
-  console.log({ file });
 
   const onUploadFile = (e) => {
     e.preventDefault();
+    e.stopPropagation();
+    setModalVisible(false);
     formData.append('file', file);
-    for (var pair of formData.entries()) {
-      console.log(pair[0] + ', ' + pair[1]);
-    }
-    fileAPI.createFile(formData).then((res) => {
-      console.log(formData);
-      console.log(res.data);
-      setImageUrl(res.data);
-    });
+    fileAPI
+      .createFile(formData)
+      .then((res) => {
+        setFile(res.data);
+        onSubmit(res.data);
+      })
+      .catch((e) => {
+        setFile(null);
+        console.error(e.response.data);
+        setErrorMessage(e.response.data);
+        onShowErrorModal();
+      });
   };
 
+  const onShowModal = (e) => {
+    e.preventDefault();
+    setModalVisible(true);
+  };
+
+  const onShowErrorModal = () => {
+    setErrorModalVisible(true);
+  };
+
+  const onCloseErrorModal = (e) => {
+    e.preventDefault();
+    setErrorModalVisible(false);
+  };
+
+  const buttons = <ActionButton onClick={onCloseErrorModal}>확인</ActionButton>;
+
   return (
-    <form encType="multipart/form-data" onSubmit={onUploadFile}>
-      <StyledFileButton
-        className="browse-file"
-        htmlFor="my_file"
-        onChange={handleChange}
+    <>
+      <FileUploadModal
+        visible={modalVisible}
+        file={file}
+        onUploadFile={onUploadFile}
+        onBrowseFile={onBrowseFile}
+        onCancel={() => setModalVisible(false)}
+      />
+      <Modal
+        contents={errorMessage}
+        buttons={buttons}
+        visible={errorModalVisible}
+        onConfirm={onCloseErrorModal}
+        onCancel={onCloseErrorModal}
+      />
+      <ActionButton
+        className="file"
+        onClick={onShowModal}
+        style={{ marginBottom: '1rem' }}
       >
-        파일 찾기
-      </StyledFileButton>
-      <FileName>{file.name}</FileName>
-      <img src={imageUrl} alt="bookImage" />
-      <StyledActionButton className="upload-file">
-        이 파일 첨부
-      </StyledActionButton>
-      <StyledFileInput type="file" onChange={handleChange} id="my_file" />
-    </form>
+        파일 첨부
+      </ActionButton>
+    </>
   );
 };
 
---
+++ src/components/common/FileUploadModal/FileUploadModal.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import {
+  ModalBlock,
+  ModalContainer,
+  HeaderBar,
+  ContentContainer,
+  ButtonContainer,
+  ModalName,
+  StyledFileButton,
+  FileName,
+  StyledActionButton,
+  StyledFileInput,
+  StyledForm,
+  FileNameContainer,
+  FileLabel,
+} from './FileUploadModal.styles.js';
+
+const FileUploadModal = ({
+  visible,
+  file,
+  onUploadFile,
+  onBrowseFile,
+  onCancel,
+}) => {
+  const [animate, setAnimate] = useState(false);
+  const [localVisible, setLocalVisible] = useState(visible);
+
+  useEffect(() => {
+    if (localVisible && !visible) {
+      setAnimate(true);
+      setTimeout(() => setAnimate(false), 250);
+    }
+    setLocalVisible(visible);
+  }, [localVisible, visible]);
+
+  if (!localVisible && !animate) return null;
+
+  return (
+    <ModalBlock disappear={!visible}>
+      <ModalContainer disappear={!visible}>
+        <HeaderBar>
+          <i className="fas fa-times" onClick={onCancel}></i>
+        </HeaderBar>
+        <ContentContainer>
+          <ModalName>파일 업로드</ModalName>
+          <StyledForm encType="multipart/form-data" onSubmit={onUploadFile}>
+            <FileNameContainer>
+              <FileLabel>선택된 파일</FileLabel>
+              <FileName>{file?.name}</FileName>
+            </FileNameContainer>
+            <ButtonContainer>
+              <StyledFileButton
+                className="browse-file"
+                htmlFor="my_file"
+                onChange={onBrowseFile}
+              >
+                파일 찾기
+              </StyledFileButton>
+              <StyledActionButton className="upload-file">
+                이 파일 첨부
+              </StyledActionButton>
+            </ButtonContainer>
+            <StyledFileInput type="file" onChange={onBrowseFile} id="my_file" />
+          </StyledForm>
+        </ContentContainer>
+      </ModalContainer>
+    </ModalBlock>
+  );
+};
+
+export default FileUploadModal;
