221831274a79077245e34419f98572c966e02874
mingd1023 <mingd1023@gamail.com>
2021-03-17 22:56:50 +0900

    Fix: post create, update 시 fileList도 같이 담아 요청 전송하도록 수정

--- src/lib/api/post.js
+++ src/lib/api/post.js
@@ -4,10 +4,10 @@ export const getPosts = (urlPath) => client.get(`/post/board/${urlPath}`);
 
 export const getPost = (postID) => client.get(`/post/${postID}`);
 
-export const createPost = ({ title, body, boardID: boardId }) =>
-  client.post('/post', { title, body, boardId });
+export const createPost = ({ title, body, boardID: boardId, fileList }) =>
+  client.post('/post', { title, body, boardId, fileList });
 
-export const updatePost = ({ title, body, postID }) =>
-  client.put(`/post/${postID}`, { title, body });
+export const updatePost = ({ title, body, postID, fileList }) =>
+  client.put(`/post/${postID}`, { title, body, fileList });
 
 export const deletePost = (postID) => client.delete(`/post/${postID}`);
