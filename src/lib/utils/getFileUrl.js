279afaaec78f1e9495925306c3ec822046484a7e
mingd1023 <mingd1023@gamail.com>
2021-03-17 22:52:35 +0900

    Fix: file url 구하는 함수 오류 수정

--- src/lib/utils/getFileUrl.js
+++ src/lib/utils/getFileUrl.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 const { REACT_APP_FILE_URL: FILE_URL } = process.env;
 
 const getFileUrl = (url) => {
-  const fileUrl = url?.includes(FILE_URL) ? url : FILE_URL + url;
+  const fileUrl =
+    url?.includes('http://') ||
+    url?.includes('https://') ||
+    url?.includes(FILE_URL)
+      ? url
+      : FILE_URL + url;
   return fileUrl;
 };
 
