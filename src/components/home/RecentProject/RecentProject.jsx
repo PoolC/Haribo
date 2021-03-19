06c89647b7eb5850617664898ee3acb8cd6b3790
mingd1023 <mingd1023@gamail.com>
2021-03-19 21:18:11 +0900

    Feat: 최근 프로젝트 옆으로 넘기는 버튼 구현

--- src/components/home/RecentProject/RecentProject.jsx
+++ src/components/home/RecentProject/RecentProject.jsx
@@ -1,19 +1,110 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ProjectCard from '../../projects/ProjectCard/ProjectCard';
-import { RecentProjectBlock, RecentProjectList, StyledLink } from './RecentProject.styles';
+import {
+  NextButton,
+  PrevButton,
+  RecentProjectBlock,
+  RecentProjectList,
+  StyledLink,
+} from './RecentProject.styles';
 
 const RecentProject = ({ projects }) => {
+  const viewport = useRef(null);
+  const target = useRef(null);
+
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    console.log(index);
+  }, [index]);
+
+  useEffect(() => {
+    console.log('hi');
+    let options = {
+      root: viewport.current,
+      threshold: 0,
+    };
+
+    const handleIntersection = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log(entry);
+        }
+        if (!entry.isIntersecting) {
+          return;
+        }
+
+        observer.unobserve(entry.target); // 기존 타겟을 unobserve 하고
+        observer.observe(target.current); // 데이터 변경된 새로운 카드 타겟을 observe 합니다.
+      });
+    };
+
+    const io = new IntersectionObserver(handleIntersection, options);
+
+    if (target.current) {
+      io.observe(target.current); // target
+    }
+
+    return () => io && io.disconnect();
+  }, [target, viewport]);
+
+  const handleClickPrev = () => {
+    setIndex(index - 1);
+    const targetCard = document.querySelector(
+      `#recent-project-card${index - 1}`,
+    );
+    targetCard.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'start',
+    });
+  };
+  const handleClickNext = () => {
+    setIndex(index + 1);
+    const targetCard = document.querySelector(
+      `#recent-project-card${index + 1}`,
+    );
+    targetCard.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'start',
+    });
+  };
+
   return (
-    <RecentProjectBlock>
-      <h3 className="project_container_title">
-        <StyledLink to="/projects">Recent Projects</StyledLink>
-      </h3>
-      <RecentProjectList>
-        {projects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
-      </RecentProjectList>
-    </RecentProjectBlock>
+    <>
+      <RecentProjectBlock>
+        {index > 0 && (
+          <PrevButton index={index} onClick={handleClickPrev}>
+            <i className="fas fa-chevron-left"></i>
+          </PrevButton>
+        )}
+        <h3 className="project_container_title">
+          <StyledLink to="/projects">Recent Projects</StyledLink>
+        </h3>
+        <RecentProjectList className="project_card_container" ref={viewport}>
+          {projects.map((project, idx) => {
+            let lastEl = index === 6;
+            return (
+              <div
+                key={project.id}
+                id={`recent-project-card${idx}`}
+                // ref={lastEl ? target : null}
+                ref={target}
+              >
+                <ProjectCard project={project} />
+                <p>{idx}</p>
+              </div>
+            );
+          })}
+        </RecentProjectList>
+        {index < 6 && (
+          <NextButton index={index} onClick={handleClickNext}>
+            <i className="fas fa-chevron-right"></i>
+          </NextButton>
+        )}
+      </RecentProjectBlock>
+    </>
   );
 };
 
--- src/components/home/RecentProject/RecentProject.styles.js
+++ src/components/home/RecentProject/RecentProject.styles.js
@@ -3,6 +3,7 @@ import colors from '../../../lib/styles/colors';
 import { Link } from 'react-router-dom';
 
 export const RecentProjectBlock = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -39,5 +40,61 @@ export const RecentProjectList = styled.ul`
   scrollbar-width: none; /* Firefox */
   &::-webkit-scrollbar {
     display: none; /* Chrome, Safari, Opera*/
+  }
+`;
+
+export const PrevButton = styled.a`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 52%;
+  left: 3.5%;
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 2rem;
+  background-color: white;
+  z-index: 10;
+  text-decoration: none;
+  font-size: 1rem;
+  color: ${colors.brown[1]};
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 20%);
+  @media (max-width: 768px) {
+    left: 2.5%;
+  }
+  @media (max-width: 576px) {
+    left: 1.5%;
+  }
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
+export const NextButton = styled.a`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 2.5rem;
+  height: 2.5rem;
+  top: 52%;
+  right: 3.5%;
+  border-radius: 2rem;
+  background-color: white;
+  z-index: 10;
+  text-decoration: none;
+  font-size: 1rem;
+  color: ${colors.brown[1]};
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 20%);
+  @media (max-width: 768px) {
+    right: 2.5%;
+  }
+  @media (max-width: 576px) {
+    right: 1.5%;
+  }
+  &:hover {
+    text-decoration: none;
   }
 `;
