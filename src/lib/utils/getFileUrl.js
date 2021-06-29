469df077cdaa8a21b05d8695d39b6bdccf02fdc3
mingd1023 <mingd1023@gamail.com>
2021-06-29 19:33:46 +0900

    Feat: 테스트용 환경변수 추가에 따른 file url 변경

--- src/lib/utils/getFileUrl.js
+++ src/lib/utils/getFileUrl.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
-const { REACT_APP_FILE_URL: FILE_URL } = process.env;
+//const { REACT_APP_FILE_URL: FILE_URL } = process.env;
+const FILE_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_FILE_URL
+    : process.env.REACT_APP_TEST_FILE_URL;
 
 const getFileUrl = (url) => {
   const fileUrl =
