b679ca9ec1ecb263c72d7a87a2907d66eff4e3aa
jinho222 <jinho020@yonsei.ac.kr>
2023-08-01 01:11:50 +0900

    feat: add button and search input to board list

--- src/components/new-board/NewBoardList.tsx
+++ src/components/new-board/NewBoardList.tsx
@@ -1,55 +1,88 @@
-import { Space, Table, Typography } from 'antd';
-import React from 'react'
+import { Button, Input, Space, Table, Typography } from 'antd';
+import React from 'react';
 import { ColumnsType } from 'antd/es/table';
 import { Link } from 'react-router-dom';
 import { MENU } from '../../constants/menus';
 import styled from 'styled-components';
+import { GoPencil } from 'react-icons/go';
 
 type DataType = {
-  key: string,
-  title: string,
-  content: string
-  createdAt: string
-}
+  key: string;
+  title: string;
+  content: string;
+  createdAt: string;
+};
 
 export default function NewBoardList() {
-  const dataSource: DataType[]= [
+  const dataSource: DataType[] = [
     {
       key: '1',
       title: '질문 있습니다',
-      content: "html은 프로그래밍 언어인가요?",
+      content: 'html은 프로그래밍 언어인가요?',
       createdAt: '2023년 7월 28일',
     },
     {
       key: '2',
       title: '질문 있습니다',
-      content: "html은 프로그래밍 언어인가요?",
+      content: 'html은 프로그래밍 언어인가요?',
       createdAt: '2023년 7월 28일',
     },
     {
       key: '3',
       title: '질문 있습니다',
-      content: "html은 프로그래밍 언어인가요?",
+      content: 'html은 프로그래밍 언어인가요?',
       createdAt: '2023년 7월 28일',
     },
   ];
 
   const columns: ColumnsType<DataType> = [
     {
-      render: (_, {title, content, key, createdAt}) => <StyledLink to={`${MENU.NEW_BOARD}/${key}`}>
-        <Space direction={"vertical"}>
-          <Typography.Title level={5}>{title}</Typography.Title>
-          <Typography.Text>{content}</Typography.Text>
-        <Typography.Text type={"secondary"}>{createdAt}</Typography.Text>
-        </Space>
-      </StyledLink>,
+      render: (_, { title, content, key, createdAt }) => (
+        <StyledLink to={`${MENU.NEW_BOARD}/${key}`}>
+          <Space direction={'vertical'}>
+            <Typography.Title level={5}>{title}</Typography.Title>
+            <Typography.Text>{content}</Typography.Text>
+            <Typography.Text type={'secondary'}>{createdAt}</Typography.Text>
+          </Space>
+        </StyledLink>
+      ),
     },
   ];
 
-  return <Table dataSource={dataSource} columns={columns} pagination={{position: ["bottomCenter"]}} showHeader={false}/>
+  return (
+    <StyledWrapper>
+      <StyledTopBox>
+        <Input.Search allowClear style={{ maxWidth: '300px' }} />
+        <Link to={`${MENU.NEW_BOARD}/write`}>
+          <Button type={'primary'} icon={<GoPencil />}>
+            글쓰기
+          </Button>
+        </Link>
+      </StyledTopBox>
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        pagination={{ position: ['bottomCenter'] }}
+        showHeader={false}
+      />
+    </StyledWrapper>
+  );
 }
 
 const StyledLink = styled(Link)`
   display: block;
   width: 100%;
-`
+`;
+
+const StyledWrapper = styled.div`
+  display: flex;
+  align-items: stretch;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const StyledTopBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
