a09747b1cb9e76fe14b6a841c42351a564ea5d68
jinho222 <jinho020@yonsei.ac.kr>
2023-09-21 13:11:59 +0900

    avoid duplicate request in useMutation

--- src/lib/api-v2/useAppMutation.ts
+++ src/lib/api-v2/useAppMutation.ts
@@ -1,3 +1,44 @@
-import { useMutation } from '@tanstack/react-query';
+import {
+  useMutation,
+  UseMutationOptions,
+  UseMutationResult,
+} from '@tanstack/react-query';
+import { useMessage } from '~/hooks/useMessage';
 
-export const useAppMutation = useMutation;
+/**
+ *  @description
+ *  - mutatin Func
+ *  - avoid duplicate request
+ *  @see https://medium.com/@jdimitrop/react-query-avoiding-duplicate-mutation-requests-38c722e7a2e9
+ * */
+export const useAppMutation = <
+  TData = unknown,
+  TError = unknown,
+  TVariables = void,
+  TContext = unknown,
+>({
+  mutationFn,
+  ...rest
+}: UseMutationOptions<TData, TError, TVariables, TContext>): UseMutationResult<
+  TData,
+  TError,
+  TVariables,
+  TContext
+> => {
+  const message = useMessage();
+  const mutationResults = useMutation(mutationFn!, {
+    onError() {
+      message.error('에러가 발생했습니다.');
+    },
+    ...rest,
+  });
+
+  return {
+    ...mutationResults,
+    mutate: (...params: [TVariables]) => {
+      if (!mutationResults.isLoading) {
+        mutationResults.mutate(...params);
+      }
+    },
+  };
+};
